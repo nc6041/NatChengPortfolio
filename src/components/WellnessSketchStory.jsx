bda316f70fce4338bb7ef42c1129f907b0372fcb
fujiason <fujiason100@gmail.com>
2023-08-26 17:55:50 -0500

    Update WellnessSketchStory.jsx

--- src/components/WellnessSketchStory.jsx
+++ src/components/WellnessSketchStory.jsx
@@ -4,12 +4,12 @@ import { sketch, storyboard } from "../assets";
 const UlineWhy = () => {
   return (
     <section className={`flex lg:flex-row flex-col ${styles.paddingY}`}>
-        <div className="flex-1 flex flex-col pb-10 sm:pr-3 pr-0">
-            <img src={sketch} alt={"sketch"} className="w-[100%] h-[100%] relative" />    
+        <div className="basis-2/5 flex flex-col pb-10 xl:pr-3 lg:pr-3 lg:pl-16 xl:px-0 sm:px-16 px-6">
+            <img src={sketch} alt={"sketch"} className="h-[100%] w-[100%] relative" />    
           </div>
 
-          <div className="flex-1 flex flex-col pb-10">
-            <img src={storyboard} alt={"storyboard"} className="w-[100%] h-[100%] relative" />    
+          <div className="basis-3/5 flex flex-col pb-10 xl:px-0 lg:pr-16 lg:px-0 sm:px-16 px-6">
+            <img src={storyboard} alt={"storyboard"} className="h-[100%] w-[100%] relative" />    
           </div>
 
     </section>
