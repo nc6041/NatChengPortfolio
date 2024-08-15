4f7c2e6d8642d4f1bf57bfb4c1f919e3d2c04abf
Zeke Jeske <zeke.jeske@gmail.com>
2024-08-15 17:41:43 +0200

    "

--- src/pages/Home/CaseCard.tsx
+++ src/pages/Home/CaseCard.tsx
@@ -20,43 +20,44 @@ const CaseCard = ({
   whatFor,
   imgSrc,
   href = `/${id}`,
-}: CaseCardProps) => (
-  <section
-    id={id}
-    className={classNames(
-      'bg-neutral-100 flex flex-col sm:flex-row rounded-md sm:rounded-3xl xl:mb-5 mb-0',
-      // For border radius
-      'overflow-hidden',
-    )}
-  >
-    <div className="justify-between gap-8 flex-1 flex flex-col p-6 xs:px-12 sm:px-16 xs:py-12">
-      <div className="flex flex-col gap-6 items-start">
-        <Title style="homepage" order={3}>
-          {title}
-        </Title>
-        <Text>{description}</Text>
+}: CaseCardProps) => {
+  const fullHref = `${href}#${topId}`
 
-        <Link href={href} style="button">
-          View Study
-        </Link>
+  return (
+    <section
+      id={id}
+      className={classNames(
+        'bg-neutral-100 flex flex-col sm:flex-row rounded-md sm:rounded-3xl xl:mb-5 mb-0',
+        // For border radius
+        'overflow-hidden',
+      )}
+    >
+      <div className="justify-between gap-8 flex-1 flex flex-col p-6 xs:px-12 sm:px-16 xs:py-12">
+        <div className="flex flex-col gap-6 items-start">
+          <Title style="homepage" order={3}>
+            {title}
+          </Title>
+          <Text>{description}</Text>
+
+          <Link href={fullHref} style="button">
+            View Study
+          </Link>
+        </div>
+
+        <Title style="homepage" order={4}>
+          {whatFor}
+        </Title>
       </div>
 
-      <Title style="homepage" order={4}>
-        {whatFor}
-      </Title>
-    </div>
-
-    <a
-      href={`${href}#${topId}`}
-      className="flex flex-1 flex-col max-w-xl self-center"
-    >
-      <img
-        src={imgSrc}
-        alt={id}
-        className="w-full h-full relative object-cover"
-      />
-    </a>
-  </section>
-)
+      <a href={fullHref} className="flex flex-1 flex-col max-w-xl self-center">
+        <img
+          src={imgSrc}
+          alt={id}
+          className="w-full h-full relative object-cover"
+        />
+      </a>
+    </section>
+  )
+}
 
 export default CaseCard
